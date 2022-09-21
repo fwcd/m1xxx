279139ea106fbd15c1d73305b5fb8a4ae7473db7
fwcd <fwcdmail@gmail.com>
2022-09-21 15:12:38 +0200

    Use vcpkg wrapper in scripts/install-vcpkg-deps

--- vars/vcpkg.sh
+++ vars/vcpkg.sh
@@ -1,6 +1,6 @@
 
 if [ -z "$VCPKG_ROOT" ]; then
-  export VCPKG_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/vcpkg"
+  export VCPKG_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd)/vcpkg"
 fi
 
 export VCPKG_OVERLAY_TRIPLETS="$VCPKG_ROOT/overlay/triplets"
