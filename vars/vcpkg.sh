ced102edb5de84e6d2492d465f47897e53b13ef3
fwcd <fwcdmail@gmail.com>
2022-10-14 18:05:16 +0200

    Reference iOS overlay ports

--- vars/vcpkg.sh
+++ vars/vcpkg.sh
@@ -5,4 +5,4 @@ if [ -z "$VCPKG_ROOT" ]; then
 fi
 
 export VCPKG_OVERLAY_TRIPLETS="$VCPKG_ROOT/overlay/triplets"
-export VCPKG_OVERLAY_PORTS="$VCPKG_ROOT/overlay/osx:$VCPKG_ROOT/overlay/ports"
+export VCPKG_OVERLAY_PORTS="$VCPKG_ROOT/overlay/ios:$VCPKG_ROOT/overlay/osx:$VCPKG_ROOT/overlay/ports"
