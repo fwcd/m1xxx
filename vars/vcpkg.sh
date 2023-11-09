e3f8ba1b636d915a9f575a6c96a61b04e1a132c4
fwcd <fwcdmail@gmail.com>
2023-11-09 22:24:26 +0000

    Add release-mode triplet for x64-linux
    Building debug symbols causes CI to fail due to a lack of space.
    
    Unfortunately, this workaround is still needed:
    https://github.com/microsoft/vcpkg/issues/10683

--- vars/vcpkg.sh
+++ vars/vcpkg.sh
@@ -4,5 +4,5 @@ if [ -z "$VCPKG_ROOT" ]; then
   export VCPKG_ROOT="$rootdir/vcpkg"
 fi
 
-export VCPKG_OVERLAY_TRIPLETS="$VCPKG_ROOT/overlay/triplets"
+export VCPKG_OVERLAY_TRIPLETS="$rootdir/overlay/triplets:$VCPKG_ROOT/overlay/triplets"
 export VCPKG_OVERLAY_PORTS="$VCPKG_ROOT/overlay/osx:$VCPKG_ROOT/overlay/ports"
