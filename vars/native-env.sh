59710bcc91696f7316b23ce39993fdb5bf293530
fwcd <fwcd@fwcd.dev>
2024-02-05 03:51:42 +0100

    Build vcpkg environment in manifest mode

--- vars/native-env.sh
+++ vars/native-env.sh
@@ -1,7 +1,7 @@
-varsdir=$(cd "$(dirname $0)" && pwd)
+varsdir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 source "$varsdir/vcpkg.sh"
 
-export VCPKG_DEFAULT_TRIPLET=arm64-osx
+export VCPKG_DEFAULT_TRIPLET=arm64-osx-release
 export CMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 export CMAKE_EXPORT_COMPILE_COMMANDS=ON
