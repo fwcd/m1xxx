7f8b132ef7677003431f0a4c53439a610828b664
fwcd <fwcd@fwcd.dev>
2024-02-10 01:04:59 +0100

    Remove explicit overlays and rely on the JSON config instead

--- vars/vcpkg.sh
+++ vars/vcpkg.sh
@@ -3,6 +3,3 @@ rootdir=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 if [ -z "$VCPKG_ROOT" ]; then
   export VCPKG_ROOT="$rootdir/vcpkg"
 fi
-
-export VCPKG_OVERLAY_TRIPLETS="$VCPKG_ROOT/overlay/triplets"
-export VCPKG_OVERLAY_PORTS="$VCPKG_ROOT/overlay/ios:$VCPKG_ROOT/overlay/osx:$VCPKG_ROOT/overlay/ports"
